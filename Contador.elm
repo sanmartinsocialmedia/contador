3d5b14c8b66469a74c5a7a581385c2a14a5d8ad7
Carlos Sanmartin <sanmartin.socialmedia@gmail.com>
2017-03-07 20:23:33 -0300

    Adicionada - Vista
    Adicionada vista com botões usando html

--- Contador.elm
+++ Contador.elm
@@ -1,5 +1,26 @@
 module Main exposing (..)
 
+import Html exposing (..)
+
+
+-- Modelo
+
 
 modelo =
     0
+
+
+
+-- Vista
+
+
+main =
+    div []
+        [ button [] [ text "-" ]
+        , text (toString modelo)
+        , button [] [ text "+" ]
+        ]
+
+
+
+-- Atualização
